cf11bafb4aa02de2ad351289d013154950c6dc9f
Leya jude <leyajude3@gmail.com>
2024-03-05 11:18:56 +0530

    inital

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './components/Counter'
+function App() {
+
+  return (
+    <>
+     <h3 className='text-center mt-5 '>Counter-App</h3>
+     <div style={{height:'50vh'}} className='d-flex justify-content-center align-items center w-100'><Counter /></div>
+
+    </>
+  )
+}
+
+export default App
